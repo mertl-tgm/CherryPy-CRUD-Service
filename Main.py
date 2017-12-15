494c306b0f267764d3bce559e41c40c48d3c3350
Lukas Zuba <lzuba@student.tgm.ac.at>
2017-12-15 11:40:41 +0100

    added db connection

--- Main.py
+++ Main.py
@@ -1,9 +1,19 @@
 import cherrypy
-
+import psycopg2
 
 class HelloWorld(object):
+
     def index(self):
-        return "Hello World!"
+        conn = psycopg2.connect("dbname='xplorg' user='xplorg' host='xplorg.at' password='GCUNyNxsBHLZwRPV3ayeSYMS'")
+        cur = conn.cursor()
+        cur.execute("Select * from Person")
+        rows = cur.fetchall()
+        res = "Hello World! <br>"
+        for row in rows:
+            print(row)
+            res += str(row)
+
+        return res
 
     index.exposed = True
 

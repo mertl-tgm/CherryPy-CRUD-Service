48647c789b776c145fbfe277be020efdb794da53
mertl-tgm <mertl@student.tgm.ac.at>
2018-01-10 12:35:43 +0100

    Added handling of inputs from updating user

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 import os.path
 import sqlite3
+from time import sleep
 
 import cherrypy
 
@@ -33,11 +34,39 @@ class CRUDWebService(object):
                 response += "</table>"
             return response
 
+        if param == "update":
+            sleep(0.05)
+            with sqlite3.connect(DB_STRING) as c:
+                r = c.execute("SELECT * FROM benutzer")
+                response = "<table width='100%' class='table table-striped table-bordered'" \
+                           "cellspacing='0'><tr><td>Nr</td><td>Vorname</td><td>Nachname</td>" \
+                           "<td>Username</td><td>Ändern</td></tr>"
+                while True:
+                    row = r.fetchone()
+                    if row is None:
+                        break
+                    response += "<tr><td>" + str(row[0]) + "</td><td>" + row[1] + "</td><td>" + row[2] + "</td><td>" \
+                                + row[3] + "</td><td> <button onClick='updateBenutzer(" + str(row[0]) + ")'>Ändern" \
+                                "</button></td></tr>"
+                response += '</table>'
+            return response
+
+        if param == "getBenutzer":
+            with sqlite3.connect(DB_STRING) as c:
+                r = c.execute("SELECT * FROM benutzer WHERE nr=" + input)
+                while True:
+                    row = r.fetchone()
+                    if row is None:
+                        break
+                    response = str(row[0]) + "#" + row[1] + "#" + row[2] + "#" + row[3] + "#" + row[4];
+            return response
+
         if param == "updateBenutzer":
             with sqlite3.connect(DB_STRING) as c:
+                print(input)
                 liste = input.split('#')
-                c.execute("update benutzer set vorname='" + liste[1] + "',nachname='"
-                          + liste[2] + "',username='" + liste[3] + "',password='" + liste[4] + "' where nr="+liste[0])
+                c.execute("UPDATE benutzer SET vorname='" + liste[1] + "',nachname='"
+                          + liste[2] + "',username='" + liste[3] + "',password='" + liste[4] + "' WHERE nr=" + liste[0])
                 return "Benutzer erfolgreich geändert."
 
         if param == "create":
@@ -105,7 +134,7 @@ if __name__ == '__main__':
 
     cherrypy.engine.subscribe('start', setup_database)
     cherrypy.engine.subscribe('stop', cleanup_database)
-    cherrypy.config.update({'server.socket_port': 32423})
+    cherrypy.config.update({'server.socket_port': 24128})
     cherrypy.config.update({'engine.autoreload_on': False})
 
     webapp = CRUD()
